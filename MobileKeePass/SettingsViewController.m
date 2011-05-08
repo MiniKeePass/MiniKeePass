a693486d6e3840833fc20437fa64ef550792f468
Jason Rush <jarush@vmmac.(none)>
2011-05-07 21:55:49 -0500

    Formatting

--- MobileKeePass/SettingsViewController.m
+++ MobileKeePass/SettingsViewController.m
@@ -39,8 +39,7 @@
     return 2;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *CellIdentifier = @"Cell";
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
