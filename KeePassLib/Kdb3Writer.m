563000119e8235e86bee733a2fc514cb7468876d
Jason Rush <jarush@vmmac.(none)>
2011-05-31 19:54:28 -0500

    Fixed crash on new file

--- KeePassLib/Kdb3Writer.m
+++ KeePassLib/Kdb3Writer.m
@@ -147,37 +147,32 @@
     KdbGroup *parentGroup = [tree createGroup:rootGroup];
     parentGroup.name = @"General";
     parentGroup.image = 48;
-    [tree.root addGroup:parentGroup];
+    [rootGroup addGroup:parentGroup];
     
     KdbGroup *group = [tree createGroup:parentGroup];
     group.name = @"Windows";
     group.image = 38;
     [parentGroup addGroup:group];
-    [group release];
     
     group = [tree createGroup:parentGroup];
     group.name = @"Network";
     group.image = 3;
     [parentGroup addGroup:group];
-    [group release];
     
     group = [tree createGroup:parentGroup];
     group.name = @"Internet";
     group.image = 1;
     [parentGroup addGroup:group];
-    [group release];
     
     group = [tree createGroup:parentGroup];
     group.name = @"eMail";
     group.image = 19;
     [parentGroup addGroup:group];
-    [group release];
     
     group = [tree createGroup:parentGroup];
     group.name = @"Homebanking";
     group.image = 37;
     [parentGroup addGroup:group];
-    [group release];
     
     [self persist:tree file:fileName withPassword:password];
     
