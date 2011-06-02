9be8839604288c5153d3fc46d1323de8a22fbbc3
Jason Rush <jarush@vmmac.(none)>
2011-06-01 20:30:45 -0500

    Fixed some headers

--- KeePassLib/Kdb4Parser.h
+++ KeePassLib/Kdb4Parser.h
@@ -1,10 +1,19 @@
-//
-//  Kdb4Parser.h
-//  KeePass2
-//
-//  Created by Qiang Yu on 2/4/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
-//
+/*
+ * Copyright 2011 Jason Rush and John Flanagan. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import <Foundation/Foundation.h>
 #import "Kdb4Node.h"
