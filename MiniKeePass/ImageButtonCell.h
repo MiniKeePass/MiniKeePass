8df7309a3ea402c86f589c450eb32337b6fca828
Jason Rush <jarush@vmmac.(none)>
2011-07-10 15:52:54 -0500

    Added an image button cell for image selection

--- MiniKeePass/ImageButtonCell.h
+++ MiniKeePass/ImageButtonCell.h
@@ -1,16 +1,28 @@
-//
-//  ImageButtonCell.h
-//  MiniKeePass
-//
-//  Created by Jason Rush on 7/10/11.
-//  Copyright 2011 Self. All rights reserved.
-//
+/*
+ * Copyright 2011 Jason Rush and John Flanagan. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import <UIKit/UIKit.h>
 
-
 @interface ImageButtonCell : UITableViewCell {
-    
+    UIButton *imageButton;
 }
+
+@property (nonatomic, retain) UIButton *imageButton;
+
+- (id)initWithLabel:(NSString*)labelText;
 
 @end
--- MiniKeePass/ImageButtonCell.m
+++ MiniKeePass/ImageButtonCell.m
@@ -1,35 +1,57 @@
-//
-//  ImageButtonCell.m
-//  MiniKeePass
-//
-//  Created by Jason Rush on 7/10/11.
-//  Copyright 2011 Self. All rights reserved.
-//
+/*
+ * Copyright 2011 Jason Rush and John Flanagan. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import "ImageButtonCell.h"
 
-
 @implementation ImageButtonCell
 
-- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
-{
-    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+@synthesize imageButton;
+
+- (id)initWithLabel:(NSString*)labelText {
+    self = [super initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
     if (self) {
         // Initialization code
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
+        
+        self.textLabel.text = labelText;
+        
+        imageButton = [[UIButton alloc] init];
+        [self addSubview:imageButton];
     }
     return self;
 }
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated
-{
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
+- (void)dealloc {
+    [imageButton release];
+    [super dealloc];
 }
 
-- (void)dealloc
-{
-    [super dealloc];
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    CGRect rect = self.contentView.frame;
+    CGSize size = CGSizeMake(24, 24);
+    
+    NSLog(@"w: %f  h: %f", size.width, size.height);
+    
+    CGFloat x = rect.origin.x + rect.size.width - size.width - 10;
+    CGFloat y = rect.origin.y + rect.size.height / 2.0 - size.height / 2.0;
+    
+    imageButton.frame = CGRectMake(x, y, size.width, size.height);
 }
 
 @end
