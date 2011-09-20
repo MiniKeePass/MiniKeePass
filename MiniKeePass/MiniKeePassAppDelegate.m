7044c24ec5f22463abab27b065c42855008780c6
John <tallerthenyou@gmail.com>
2011-09-20 17:55:23 -0500

    Changed default settings to make app more secure without requiring use to make settings changes

--- MiniKeePass/MiniKeePassAppDelegate.m
+++ MiniKeePass/MiniKeePassAppDelegate.m
@@ -61,7 +61,7 @@ static NSStringEncoding passwordEncodingValues[] = {
     [defaultsDict setValue:[NSNumber numberWithInt:1] forKey:@"deleteOnFailureAttempts"];
     [defaultsDict setValue:[NSNumber numberWithBool:NO] forKey:@"closeEnabled"];
     [defaultsDict setValue:[NSNumber numberWithInt:1] forKey:@"closeTimeout"];
-    [defaultsDict setValue:[NSNumber numberWithBool:YES] forKey:@"rememberPasswordsEnabled"];
+    [defaultsDict setValue:[NSNumber numberWithBool:NO] forKey:@"rememberPasswordsEnabled"];
     [defaultsDict setValue:[NSNumber numberWithBool:YES] forKey:@"hidePasswords"];
     [defaultsDict setValue:[NSNumber numberWithInt:0] forKey:@"passwordEncoding"];
     [defaultsDict setValue:[NSNumber numberWithBool:NO] forKey:@"clearClipboardEnabled"];
