1f62c4cd87c6cd17350b0bc3c23f8e77497776b7
John Flanagan <john@Johns-MacBook-Pro-2.local>
2011-10-19 10:57:51 -0500

    changed default vaules more to add more security

--- MiniKeePass/MiniKeePassAppDelegate.m
+++ MiniKeePass/MiniKeePassAppDelegate.m
@@ -59,8 +59,8 @@ static NSStringEncoding passwordEncodingValues[] = {
     [defaultsDict setValue:[NSNumber numberWithInt:1] forKey:@"pinLockTimeout"];
     [defaultsDict setValue:[NSNumber numberWithBool:NO] forKey:@"deleteOnFailureEnabled"];
     [defaultsDict setValue:[NSNumber numberWithInt:1] forKey:@"deleteOnFailureAttempts"];
-    [defaultsDict setValue:[NSNumber numberWithBool:NO] forKey:@"closeEnabled"];
-    [defaultsDict setValue:[NSNumber numberWithInt:1] forKey:@"closeTimeout"];
+    [defaultsDict setValue:[NSNumber numberWithBool:YES] forKey:@"closeEnabled"];
+    [defaultsDict setValue:[NSNumber numberWithInt:4] forKey:@"closeTimeout"];
     [defaultsDict setValue:[NSNumber numberWithBool:NO] forKey:@"rememberPasswordsEnabled"];
     [defaultsDict setValue:[NSNumber numberWithBool:YES] forKey:@"hidePasswords"];
     [defaultsDict setValue:[NSNumber numberWithInt:0] forKey:@"passwordEncoding"];
