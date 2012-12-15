dbd1aa7cd5b1d647b984770af10a8d0af9ccf4de
Jason Rush <jason@fizzawizza.com>
2012-12-15 12:42:52 -0600

    Converting to new stype property

--- KeePassLib/Kdb3Node.m
+++ KeePassLib/Kdb3Node.m
@@ -11,38 +11,34 @@
 
 @implementation Kdb3Group
 
-@synthesize groupId;
-@synthesize flags;
-@synthesize metaEntries;
-
 - (id)init {
     self = [super init];
     if (self) {
-        metaEntries = [[NSMutableArray alloc] initWithCapacity:4];
+        _metaEntries = [[NSMutableArray alloc] init];
     }
     return self;
 }
 
 - (void)dealloc {
-    [metaEntries release];
+    [_metaEntries release];
     [super dealloc];
 }
 
-- (void)addEntry:(KdbEntry*)entry {
+- (void)addEntry:(KdbEntry *)entry {
     entry.parent = self;
     
-    if ([(Kdb3Entry*)entry isMeta]) {
-        [metaEntries addObject:entry];
+    if ([(Kdb3Entry *)entry isMeta]) {
+        [_metaEntries addObject:entry];
     } else {
         [entries addObject:entry];
     }
 }
 
-- (void)removeEntry:(KdbEntry*)entry {
+- (void)removeEntry:(KdbEntry *)entry {
     entry.parent = nil;
     
-    if ([(Kdb3Entry*)entry isMeta]) {
-        [metaEntries removeObject:entry];
+    if ([(Kdb3Entry *)entry isMeta]) {
+        [_metaEntries removeObject:entry];
     } else {
         [entries removeObject:entry];
     }
@@ -53,44 +49,35 @@
 
 @implementation Kdb3Entry
 
-@synthesize uuid;
-@synthesize title;
-@synthesize username;
-@synthesize password;
-@synthesize url;
-@synthesize notes;
-@synthesize binaryDesc;
-@synthesize binary;
-
 - (void)dealloc {
-    [uuid release];
-    [title release];
-    [username release];
-    [password release];
-    [url release];
-    [notes release];
-    [binaryDesc release];
-    [binary release];
+    [_uuid release];
+    [_title release];
+    [_username release];
+    [_password release];
+    [_url release];
+    [_notes release];
+    [_binaryDesc release];
+    [_binary release];
     [super dealloc];
 }
 
 - (BOOL)isMeta {
-    if (!binary || binary.length == 0) {
+    if (!_binary || _binary.length == 0) {
         return NO;
     }
-    if (!notes || ![notes length]) {
+    if (!_notes || ![_notes length]) {
         return NO;
     }
-    if (!binaryDesc || [binaryDesc compare:@"bin-stream"]) {
+    if (!_binaryDesc || [_binaryDesc compare:@"bin-stream"]) {
         return NO;
     }
-    if (!title || [title compare:@"Meta-Info"]) {
+    if (!_title || [_title compare:@"Meta-Info"]) {
         return NO;
     }
-    if (!username || [username compare:@"SYSTEM"]) {
+    if (!_username || [_username compare:@"SYSTEM"]) {
         return NO;
     }
-    if (!url || [url compare:@"$"]) {
+    if (!_url || [_url compare:@"$"]) {
         return NO;
     }
     if (image) {
@@ -104,12 +91,10 @@
 
 @implementation Kdb3Tree
 
-@synthesize rounds;
-
 - (id)init {
     self = [super init];
     if (self) {
-        self.rounds = DEFAULT_TRANSFORMATION_ROUNDS;
+        _rounds = DEFAULT_TRANSFORMATION_ROUNDS;
     }
     return self;
 }
