f7227217483cbe3b114faa6884a493cb5bddc557
CooperRS <CooperRS@yasit.net>
2013-12-07 11:57:36 +0100

    Bugfixing

--- MiniKeePass/PasswordViewController.m
+++ MiniKeePass/PasswordViewController.m
@@ -65,6 +65,8 @@
     [super viewWillAppear:animated];
     
     [self.tableView deselectRowAtIndexPath:[self.tableView indexPathForSelectedRow] animated:YES];
+    
+    [self.passwordTextField becomeFirstResponder];
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
