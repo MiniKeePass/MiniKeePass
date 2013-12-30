cb9df8c9025368618cb5d3ff9e68e28716865247
Jason Rush <jason@fizzawizza.com>
2013-12-29 20:29:15 -0600

    Added missing file headers

--- MiniKeePass/WebViewController.m
+++ MiniKeePass/WebViewController.m
@@ -1,10 +1,19 @@
-//
-//  WebViewController.m
-//  MiniKeePass
-//
-//  Created by Jason Rush on 1/25/13.
-//  Copyright (c) 2013 Self. All rights reserved.
-//
+/*
+ * Copyright 2011-2013 Jason Rush and John Flanagan. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import "WebViewController.h"
 
