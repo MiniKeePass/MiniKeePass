e4208a5af7a1bade795a7b8ea4b6c31f8d2c5efa
Ryan Punt <ryan@mirum.org>
2014-01-27 15:17:39 -0600

    Added tooltips to DatabaseManager.h

--- MiniKeePass/DatabaseManager.h
+++ MiniKeePass/DatabaseManager.h
@@ -20,9 +20,15 @@
 
 @interface DatabaseManager : NSObject
 
+/// A string containing the name of the KeePass DatabaseDocument to be managed
 @property (nonatomic, copy) NSString *selectedFilename;
 
+/// Create a DatabaseManager instance
 + (DatabaseManager*)sharedInstance;
+
+/// Open the specified KeePass DatabaseDocument
+/// @param path Path to the chosen KeePass DatabaseDocument
+/// @param animated Animate the ViewController transition
 - (void)openDatabaseDocument:(NSString*)path animated:(BOOL)newAnimated;
 
 @end
