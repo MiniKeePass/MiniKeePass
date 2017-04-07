2723ffaee033dcd403d6747ee0c2b7e3f96d3f37
Tait Smith <tssmith@Taits-MacBook-Pro.local>
2017-04-07 07:04:46 -0700

    Changed VariantDicitonary to keep track of the type of number contained b/c NSNumber casts everything to long.  Added a converter from uint64 to little endian bytes.

--- KeePassLib/Utils.h
+++ KeePassLib/Utils.h
@@ -9,6 +9,14 @@
 #import <Foundation/Foundation.h>
 #import "InputStream.h"
 
+#define VARIANT_DICT_TYPE_UINT32      0x04
+#define VARIANT_DICT_TYPE_UINT64      0x05
+#define VARIANT_DICT_TYPE_BOOL        0x08
+#define VARIANT_DICT_TYPE_INT32       0x0C
+#define VARIANT_DICT_TYPE_INT64       0x0D
+#define VARIANT_DICT_TYPE_STRING      0x18
+#define VARIANT_DICT_TYPE_BYTEARRAY   0x42
+
 @interface Utils : NSObject {
 }
 
@@ -22,15 +30,17 @@
 + (NSData*)getUInt64Bytes:(uint64_t)val;
 + (NSData*)getUInt32Bytes:(uint32_t)val;
 
++ (uint64_t)BytesToInt64:(NSData*)data;
 @end
 
 @interface VariantDictionary : NSObject {
     NSMutableDictionary *dict;
+    NSMutableDictionary *type;
 }
 
 -(id) init;
 
-- (void)setObject:(id)obj forKeyedSubscript:(id <NSCopying>)key;
+- (void)addObject:(id)obj forKey:(id <NSCopying>)key objtype:(uint32_t)objtype;
 - (id)objectForKeyedSubscript:(id)key;
 
 -(void) deserialize:(InputStream*)data;
